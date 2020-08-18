8b86368983ab7ca1995ef79419c9a723dccd3bc1
Velin Georgiev <velin.georgiev@gmail.com>
2020-08-18 07:53:09 +0100

    fluent added

--- src/webparts/helloWorld/components/HelloWorld.tsx
+++ src/webparts/helloWorld/components/HelloWorld.tsx
@@ -13,16 +13,17 @@ import Button from '@material-ui/core/Button';
 // DONT
 // const Material: any = require('@material-ui/core');
 // import * as Material from '@material-ui/core'
+// import * as fluent from '@fluentui/react';
 // import '@material-ui/core'
 
 export default class HelloWorld extends React.Component<IHelloWorldProps, {}> {
 
   public componentWillMount() {
-      // let count = 10000;
-      // while(count--) 
-      // { 
-      //   console.log("blocked");
-      // }
+      let count = 10000;
+      while(count--) 
+      { 
+        console.log("blocked");
+      }
   }
 
   public componentDidMount() {
@@ -37,9 +38,12 @@ export default class HelloWorld extends React.Component<IHelloWorldProps, {}> {
           <div className={ styles.row }>
             <div className={ styles.column }>
               <span className={ styles.title }>Welcome to SharePoint!</span>
-              <Button variant="contained" color="secondary">
-                Secondary
-              </Button>
+              <div style={{padding: '2rem'}}>
+                <Button variant="contained" color="secondary">
+                  Button
+                </Button>
+              </div>
+              {/* <fluent.PrimaryButton>Button 3</fluent.PrimaryButton> */}
               {/* <UserProfile /> */}
               {/* <UserProfileCache /> */}
               <UserProfileTimes />
